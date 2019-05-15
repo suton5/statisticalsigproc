4a4159b9c6f4bc0b68ae8a85992ea78a278ee802
Sujay Thakur <sujay@user1292.eduroam.christs.private.cam.ac.uk>
2019-05-15 16:47:30 +0100

    Finished qns 1-7

--- 1/freqisolation.m
+++ 1/freqisolation.m
@@ -1,32 +1,34 @@
-% Plot sound files
-%x=audioread('audio/alphabet/A.wav');
-%xt=x(1200:3500,1);
-%x=audioread('audio/piano_clean.wav');
-%xt=x(12100:12300,1);
-[x, Fs]=audioread('audio/piano_clean.wav');
-xt=x(12100:12900,1);
+% 5th note in piano extact
+% x=audioread('audio/piano_clean.wav');
+% xt=x(12100:12900,1);
+% plot(xt)
+
+% 6th note in piano extact
+x=audioread('audio/piano_clean.wav');
+xt=x(14900:15700,1);
+% plot(xt)
+
+% The consonant 'J' in the word 'John'
+% [x, Fs]=audioread('audio/f1lcapae.wav');
+% xt=x(4500:6001,1);
+% plot(xt)
+
+% The vowel 'I' in the word 'resigned'
+% [x, Fs]=audioread('audio/f1lcapae.wav');
+% xt=x(11000:13501,1);
+% plot(xt) 
+
+% Compute length of truncated sequence
 N=size(xt,1);
-% xhamm=xt.*hamming(N);
-% xhann=xt.*hanning(N);
-% xcheb=xt.*chebwin(N);
-%fvtool(xt,1,xhamm,2,xhann,3,xcheb,4)
 
 % Plot spectrum
-%audiofft(xt, 'rect', N);
-%hold on
 xw=xt.* hamming(N);
 Y = fft(xw);
 P2 = abs(Y/N);
 P1 = P2(1:N/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 f = Fs*(0:(N/2))/N;
-plot(f,P1) 
+plot(f,P1)
 title('Single-Sided Frequency Spectrum')
 xlabel('f (Hz)')
 ylabel('Amplitude')
-
-% audiofft(xt, 'hamm', N);
-% %audiofft(xt, 'hamm', N);
-%audiofft(xt, 'cheb', N);
-%legend('rect','hamm','hann','cheb')
-%hold off
