47d9920b23ce72efbc26b7f780fa260e57c65b4a
Sujay Thakur <suton5@gmail.com>
2019-05-31 17:43:50 +0100

    Generate posterior model choice distributions

--- 3/Q3.m
+++ 3/Q3.m
@@ -1,5 +1,4 @@
 clear all;
-rng(1,'twister');
 
 % Select a value for theta and data size
 theta2=5;
@@ -22,14 +21,14 @@ theta_ML3 = inv(transpose(G3)*G3)*transpose(G3)*y3;
 % Prior distribution parameters
 theta_prior2 = 0;
 theta_prior3 = [0;0];
-theta_var = 10000000000;
+theta_var = 10^(15);
 prior_var2 = theta_var;
 prior_var3 = theta_var*eye(2);
 
 % Model marginal log likelihoods (prevent underflow issues)
-model_11 = logmultigaussian(N,y1,0,error_var*eye(N))
-model_21 = model_marginal_loglikelihood(N,1,G2,error_var,theta_prior2,prior_var2,y1)
-model_31 = model_marginal_loglikelihood(N,2,G3,error_var,theta_prior3,prior_var3,y1)
+model_11 = logmultigaussian(N,y1,0,error_var*eye(N));
+model_21 = model_marginal_loglikelihood(N,1,G2,error_var,theta_prior2,prior_var2,y1);
+model_31 = model_marginal_loglikelihood(N,2,G3,error_var,theta_prior3,prior_var3,y1);
 
 model_12 = logmultigaussian(N,y2,0,error_var*eye(N));
 model_22 = model_marginal_loglikelihood(N,1,G2,error_var,theta_prior2,prior_var2,y2);
@@ -38,3 +37,4 @@ model_32 = model_marginal_loglikelihood(N,2,G3,error_var,theta_prior3,prior_var3
 model_13 = logmultigaussian(N,y3,0,error_var*eye(N));
 model_23 = model_marginal_loglikelihood(N,1,G2,error_var,theta_prior2,prior_var2,y3);
 model_33 = model_marginal_loglikelihood(N,2,G3,error_var,theta_prior3,prior_var3,y3);
+BF23=model_23-model_33
--- 3/Q5.m
+++ 3/Q5.m
@@ -1,98 +1,169 @@
+clear all;
+rng(2,'twister');
+
 N=100;
-error_var=0.5;
+error_var=0.5G;
 pole1=0.5+0.1j;
 pole2=0.5-0.1j;
 a1=pole1+pole2;
 a2=-pole1*pole2;
+% a1=0.1;
+% a2=-0.2;
+a3=0.3;
 
-signal_AR=[0.01 0.01];
+signal_AR=[0.1 0.2];
 for i=2:N-1
     next=a1*signal_AR(i) + a2*signal_AR(i-1) + sqrt(error_var)*randn(1,1);
     signal_AR=[signal_AR next];
 end
+
+% signal_AR=[0.1 0.2 0.1];
+% for i=3:N-1
+%     next=a1*signal_AR(i) + a2*signal_AR(i-1) + a3*signal_AR(i-2) + sqrt(error_var)*randn(1,1);
+%     signal_AR=[signal_AR next];
+% end
+
 
 % spec = abs(fft(signal_AR));
 % figure(2)
 % plot(spec)
 % xlim([-N/2, 3*N/2])
 
-% P=2
-g1=signal_AR(2:N-1)';
-g2=signal_AR(1:N-2)';
-y=signal_AR(3:N)';
-G=[g1 g2];
+% % P=2
+% y=signal_AR(3:N)';
+% G=fliplr(buffer(signal_AR(1:end-1), N-2, N-3, 'nodelay'));
+% 
+% % ML estimate for theta
+% theta_ML = inv(transpose(G)*G)*transpose(G)*y
+% 
+% % Prior distribution parameters
+% 
+% % Setting prior as 0s and being very confident about it basically means 
+% % that MAP tries to balance between the clearly non-zero data and the zero
+% % prior estimation.
+% theta_prior = [0;0];
+% prior_var = eye(2);
+% 
+% % Likelihood parameters
+% likelihood_var = error_var*inv((transpose(G)*G));
+% 
+% % Posterior parameters
+% [theta_MAP,phi,big_theta,post_var] = post_param(G,error_var,theta_prior,prior_var,y);
+% theta_MAP
+% 
+% y_ML = G*theta_ML;
+% y_MAP = G*theta_MAP;
+% 
+% figure(1)
+% plot(y)
+% hold on
+% plot(y_ML)
+% plot(y_MAP)
+% hold off
+% title(["P=2, (ML MSE, MAP MSE)=" num2str(immse(y, y_ML)) ...
+%     num2str(immse(y, y_MAP))])
+% legend('Original', 'ML', 'MAP')
+% 
+% % P=10
+% y=signal_AR(11:N)';
+% G=fliplr(buffer(signal_AR(1:end-1), N-10, N-11, 'nodelay'));
+% 
+% % ML estimate for theta
+% theta_ML = inv(transpose(G)*G)*transpose(G)*y
+% 
+% % Prior distribution parameters
+% theta_prior = zeros(10,1);
+% prior_var = eye(10);
+% 
+% % Likelihood parameters
+% likelihood_var = error_var*inv((transpose(G)*G));
+% 
+% % Posterior parameters
+% [theta_MAP,phi,big_theta,post_var] = post_param(G,error_var,theta_prior,prior_var,y);
+% theta_MAP
+% 
+% y_ML = G*theta_ML;
+% y_MAP = G*theta_MAP;
+% 
+% figure(2)
+% plot(y)
+% hold on
+% plot(y_ML)
+% plot(y_MAP)
+% hold off
+% title(["P=10, (ML MSE, MAP MSE)=" num2str(immse(y, y_ML)) ...
+%     num2str(immse(y, y_MAP))])
+% legend('Original', 'ML', 'MAP')
 
-% ML estimate for theta
-theta_ML = inv(transpose(G)*G)*transpose(G)*y
+% % P=4
+% y=signal_AR(5:N)';
+% G=fliplr(buffer(signal_AR(1:end-1), 96, 95, 'nodelay'));
+% 
+% % ML estimate for theta
+% theta_ML = inv(transpose(G)*G)*transpose(G)*y
+% 
+% % Prior distribution parameters
+% theta_prior = zeros(4,1);
+% prior_var = eye(4);
+% 
+% % Likelihood parameters
+% likelihood_var = error_var*inv((transpose(G)*G));
+% 
+% % Posterior parameters
+% [theta_MAP,phi,big_theta,post_var] = post_param(G,error_var,theta_prior,prior_var,y);
+% theta_MAP
+% 
+% y_real = signal_AR(4:N-1)';
+% y_ML = G*theta_ML;
+% y_MAP = G*theta_MAP;
+% 
+% figure(2)
+% plot(y_real)
+% hold on
+% plot(y_ML)
+% plot(y_MAP)
+% hold off
+% title(["P=4, (ML MSE, MAP MSE)=" num2str(immse(y_real, y_ML)) ...
+%     num2str(immse(y_real, y_MAP))])
+% legend('Original', 'ML', 'MAP')
 
-% Prior distribution parameters
+% % If using proper priors
+% model_LL_list=[model_marginal_loglikelihood(N,1,signal_AR(1:99)',error_var,1,1,signal_AR(2:end)')];
 
-% Setting prior as 0s and being very confident about it basically means 
-% that MAP tries to balance between the clearly non-zero data and the zero
-% prior estimation.
-theta_prior = [0;0];
-prior_var = eye(2);
+model_LL_list=[];
+model_MLMSE_list=[];
+model_MAPMSE_list=[];
+for i = 1:10
+    G = fliplr(buffer(signal_AR(1:end-1), N-i, N-1-i, 'nodelay'));
+    
+    y=signal_AR(i+1:end)';
+    
+    % ML estimate for theta
+    theta_ML = inv(transpose(G)*G)*transpose(G)*y;
 
-% Likelihood parameters
-likelihood_var = error_var*inv((transpose(G)*G));
+    % Prior distribution parameters
+    theta_prior = zeros(i,1);
+    prior_var = eye(i);
 
-% Posterior parameters
-[theta_MAP,phi,big_theta,post_var] = post_param(G,error_var,theta_prior,prior_var,y);
-theta_MAP
+    % Posterior parameters
+    [theta_MAP,phi,big_theta,post_var] = post_param(G,error_var,theta_prior,prior_var,y);
+    model_LL = model_marginal_loglikelihood(N,i,G,error_var,zeros(i,1),eye(i),signal_AR(i+1:end)');
+%     model_LL = model_marginal_loglikelihood(N,i,G,error_var,[1; -0.26; zeros(i-2,1)],eye(i),signal_AR(i+1:end)');
+    model_LL_list = [model_LL_list; model_LL];
+    y_ML = G*theta_ML;
+    y_MAP = G*theta_MAP;
+    model_MLMSE_list = [model_MLMSE_list immse(y, y_ML)];
+    model_MAPMSE_list = [model_MAPMSE_list immse(y, y_MAP)];
+end
 
-y_real = G*[a1;a2];
-y_ML = G*theta_ML;
-y_MAP = G*theta_MAP;
-
+[best_value, best_hyperparam] = max(model_LL_list);
+probs = exp(model_LL_list);
+probs_norm = probs/sum(probs);
 figure(1)
-plot(y_real)
-hold on
-plot(y_ML)
-plot(y_MAP)
-hold off
-title(["P=2, (ML MSE, MAP MSE)=" num2str(immse(y_real, y_ML)) ...
-    num2str(immse(y_real, y_MAP))])
-legend('Original', 'ML', 'MAP')
-
-% P=10
-g1=signal_AR(10:N-1)';
-g2=signal_AR(9:N-2)';
-g3=signal_AR(8:N-3)';
-g4=signal_AR(7:N-4)';
-g5=signal_AR(6:N-5)';
-g6=signal_AR(5:N-6)';
-g7=signal_AR(4:N-7)';
-g8=signal_AR(3:N-8)';
-g9=signal_AR(2:N-9)';
-g10=signal_AR(1:N-10)';
-
-y=signal_AR(11:N)';
-G=[g1 g2 g3 g4 g5 g6 g7 g8 g9 g10];
-
-% ML estimate for theta
-theta_ML = inv(transpose(G)*G)*transpose(G)*y
-
-% Prior distribution parameters
-theta_prior = zeros(10,1);
-prior_var = eye(10);
-
-% Likelihood parameters
-likelihood_var = error_var*inv((transpose(G)*G));
-
-% Posterior parameters
-[theta_MAP,phi,big_theta,post_var] = post_param(G,error_var,theta_prior,prior_var,y);
-theta_MAP
-
-y_real = G*[a1;a2;zeros(8,1)];
-y_ML = G*theta_ML;
-y_MAP = G*theta_MAP;
-
+plot(1:10,probs_norm)
 figure(2)
-plot(y_real)
-hold on
-plot(y_ML)
-plot(y_MAP)
+plot(1:10,model_MLMSE_list)
+hold on 
+plot(1:10,model_MAPMSE_list)
 hold off
-title(["P=10, (ML MSE, MAP MSE)=" num2str(immse(y_real, y_ML)) ...
-    num2str(immse(y_real, y_MAP))])
-legend('Original', 'ML', 'MAP')
+legend('ML MSEs', 'MAP MSEs')
